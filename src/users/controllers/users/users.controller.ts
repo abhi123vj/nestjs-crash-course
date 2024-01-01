0355b29374bba6172d816c3a31fe595f6551227c
4bh16am <120167005+4bh1zd@users.noreply.github.com>
2024-01-01 21:39:55 +0530

    query params

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 
 @Controller('users')
@@ -6,6 +6,14 @@ export class UsersController {
   @Get()
   getUsers() {
     return { userName: 'Abhiram', email: '4bh1ram@gmail.com' };
+  }
+  @Get(':id/isActive')
+  getUserIsActive(@Query('active') active: string) {
+    return { active, userName: 'Abhiram', email: '4bh1ram@gmail.com' };
+  }
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return { id, userName: 'Abhiram', email: '4bh1ram@gmail.com' };
   }
   @Get('posts')
   getUsersPosts() {
